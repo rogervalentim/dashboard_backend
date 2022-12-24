605ff83845aed7b31d88e9d71849dc2cece4b471
rogervalentim <valentim.roger33@gmail.com>
2022-12-24 09:52:35 -0300

    add OverallStat model

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ import User from './models/User.js';
 import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from './data/index.js';
+import OverallStat from './models/OverallStat.js';
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from './data/index.js';
 
 // Configuration
 dotenv.config();
@@ -47,5 +48,6 @@ mongoose.connect(process.env.MONGO_URL, {
     // ProductStat.insertMany(dataProductStat);
     // User.insertMany(dataUser);
     // Transaction.insertMany(dataTransaction)
+    // OverallStat.insertMany(dataOverallStat);
     
 }).catch((error) => console.log(`${error} did not connect`))
